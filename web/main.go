e671cbccccb552374b3d16782745d0172511a305
jewoos2921 <jewoos2921@gmail.com>
2022-05-01 19:51:27 +0900

    ch6 fin

--- web/main.go
+++ web/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
+	"net/http"
+)
 
+// 모든 투표를 보여주는 index.html
+// 특정 투표 결과를 보여주는 view.html
+// 사용자가 새로운 투표를 만들 수 있는 new.html
+
+func main() {
+	var addr = flag.String("addr", ":8081", "website address")
+	flag.Parse()
+	mux := http.NewServeMux()
+	mux.Handle("/", http.StripPrefix("/",
+		http.FileServer(http.Dir("public"))))
+	log.Println("Serving website at:", *addr)
+	http.ListenAndServe(*addr, mux)
 }
